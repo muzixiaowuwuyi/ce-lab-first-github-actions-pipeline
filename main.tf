8eb9883cc0503ffe0501120c435e3c7a135e4297
Li烂石头头 <guangzheng.li2018@gmail.com>
2026-05-19 09:41:36 +0200

    Add lifecycle rule for cost optimization

--- main.tf
+++ main.tf
@@ -52,3 +52,26 @@ resource "aws_s3_bucket_public_access_block" "app_assets" {
   ignore_public_acls      = true
   restrict_public_buckets = true
 }
+
+resource "aws_s3_bucket_lifecycle_configuration" "app_assets" {
+  bucket = aws_s3_bucket.app_assets.id
+ 
+  rule {
+    id     = "expire-old-versions"
+    status = "Enabled"
+ 
+    noncurrent_version_expiration {
+      noncurrent_days = 90
+    }
+ 
+    transition {
+      days          = 30
+      storage_class = "STANDARD_IA"
+    }
+ 
+    transition {
+      days          = 60
+      storage_class = "GLACIER"
+    }
+  }
+}
